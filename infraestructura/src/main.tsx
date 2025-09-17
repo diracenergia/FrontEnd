320c2e413feaf3f5d7a07e45b2767fdae8b7a7cb
DIRAC <energiadirac@gmail.com>
2025-09-17 09:50:49 -0300

    Update main.tsx

--- infraestructura/src/main.tsx
+++ infraestructura/src/main.tsx
@@ -1,11 +1,10 @@
-// src/main.tsx
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App"; // ⬅️ default import (sin llaves)
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
